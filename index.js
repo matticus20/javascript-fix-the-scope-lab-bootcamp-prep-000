d08ff47085d44637d47a600556ef4b75e4abb2d7
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 01:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ function add2(n) {
   return n + two
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
-}
-
 function theFunk() {
   var funkyFunction = "FUNKY!"
   return funkyFunction
